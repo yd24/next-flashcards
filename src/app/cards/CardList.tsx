2466ccc8ebefb042459bc3684ba249dfc20e5f25
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-07-19 18:14:53 -0700

    Add ability to delete flashcards, handle errors for adding and deleting

--- src/app/cards/CardList.tsx
+++ src/app/cards/CardList.tsx
@@ -1,67 +1,88 @@
-'use client';
+'use client'
 
-import { Button, Stack, Flex, List, ListItem } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Button, Stack, Flex, List, ListItem } from '@chakra-ui/react'
+import { useState, MouseEvent } from 'react'
 
 type Card = {
-  id: string,
-  question: string,
-  answer: string,
-  learned: boolean,
-};
+    id: string
+    question: string
+    answer: string
+    learned: boolean
+}
 
 type Props = {
-  cards: {
-    id: string,
-    question: string,
-    answer: string,
-    learned: boolean,
-  }[],
-  createCard: () => any,
-};
+    cards: {
+        id: string
+        question: string
+        answer: string
+        learned: boolean
+    }[]
+    createCard: () => any
+    deleteCard: (id: string) => any
+}
 
 export function CardList(props: Props) {
-  const [cardsList, setCardsList] = useState(props.cards);
-  const [selectedCard, setSelectedCard] = useState('');
+    const [cardsList, setCardsList] = useState(props.cards)
+    const [selectedCardID, setSelectedCardID] = useState('')
+    const [cardError, setCardError] = useState({ error: '' })
 
-  const createCardHandler = async() => {
-    let createdCard = await props.createCard();
-    setCardsList([...cardsList, createdCard]);
-  };
-
-  const selectCardHandler = (e) => {
-    console.log(e.target.id);
-    setSelectedCard(e.target.id);
-  };
-
-  const cardItemCSS = 'p-3 cursor-pointer';
-
-  return (
-    <Stack className='p-6' spacing={6}>
-      <List className='p-4'>
-        {
-          cardsList.map(card => (
-            <ListItem 
-              key={card.id}
-              id={card.id}
-              onClick={selectCardHandler}
-              className={selectedCard === card.id ? `${cardItemCSS} bg-green-400` : `${cardItemCSS} hover:bg-green-400`}
-            >
-              {card.question}
-            </ListItem>
-          ))
+    const createCardHandler = async () => {
+        try {
+            let createdCard = await props.createCard()
+            setCardsList([...cardsList, createdCard])
+            setCardError({ error: '' })
+        } catch (e) {
+            console.error(e)
+            setCardError({ error: 'Error creating card' })
         }
-      </List>
-      <Flex justify='space-between'>
-        <Button
-          onClick={createCardHandler}
-        >
-          Add Card
-        </Button>
-        <Button>
-          Delete Card
-        </Button>
-      </Flex>
-    </Stack>
-  );
+    }
+
+    const deleteCardHandler = async (e: any) => {
+        try {
+            await props.deleteCard(selectedCardID)
+            let updatedCardsList = [...cardsList]
+            updatedCardsList.splice(
+                cardsList.findIndex((card) => card.id === selectedCardID),
+                1
+            )
+            setCardsList(updatedCardsList)
+            setSelectedCardID('')
+            setCardError({ error: '' })
+        } catch (e) {
+            console.error(e)
+            setCardError({ error: 'Error deleting card' })
+        }
+    }
+
+    const selectCardHandler = (e: any) => {
+        setSelectedCardID(e.target.id)
+    }
+
+    const cardItemCSS = 'p-3 cursor-pointer'
+
+    return (
+        <Stack className="p-6" spacing={6}>
+            <List className="p-4">
+                {cardsList.map((card) => (
+                    <ListItem
+                        key={card.id}
+                        id={card.id}
+                        onClick={selectCardHandler}
+                        className={
+                            selectedCardID === card.id
+                                ? `${cardItemCSS} bg-green-400`
+                                : `${cardItemCSS} hover:bg-green-400`
+                        }
+                    >
+                        {card.question}
+                    </ListItem>
+                ))}
+            </List>
+            <Flex justify="space-between">
+                <Button onClick={createCardHandler}>Add Card</Button>
+                <Button onClick={deleteCardHandler}>Delete Card</Button>
+            </Flex>
+            {cardError.error.length > 0 && <p className="text-red-700">{cardError.error}</p>}
+        </Stack>
+    )
 }
--- src/app/cards/ListContainer.tsx
+++ src/app/cards/ListContainer.tsx
@@ -1,31 +1,38 @@
-import { CardList } from './CardList';
-import { CardData } from './CardData';
-import { prisma } from '../../db';
+import { CardList } from './CardList'
+import { CardData } from './CardData'
+import { prisma } from '../../db'
 
 export async function ListContainer() {
     const cards = await prisma.card.findMany();
 
-    const createCard = async() => {
+    const createCard = async () => {
       'use server'
+
       const createdCard = await prisma.card.create({
-        data: {
-          question: 'New Flashcard',
-          answer: 'Fill out your answer here.',
-          learned: false,
-        }
+          data: {
+              question: 'New Flashcard',
+              answer: 'Fill out your answer here.',
+              learned: false,
+          },
       });
       return createdCard;
-    };
+    }
 
-    const deleteCard = () => {
-    };
+    const deleteCard = async(cardID: string) => {
+      'use server'
+
+      const deletedCard = await prisma.card.delete({
+          where: { id: cardID },
+      });
+      return deletedCard;
+    }
 
     return (
-      <div className="flex items-center grow">
-        <div className="flex justify-flex-start items-flex-start grow px-12">
-            <CardList cards={cards} createCard={createCard}/>
-            <CardData />
+        <div className="flex items-center grow">
+            <div className="flex justify-flex-start items-flex-start grow px-12">
+                <CardList cards={cards} createCard={createCard} deleteCard={deleteCard} />
+                <CardData />
+            </div>
         </div>
-      </div>
     )
 }
