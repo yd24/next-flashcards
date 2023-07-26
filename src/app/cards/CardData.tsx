16353151b8761ba1003df0842e4c2bd0a470eb7f
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-07-26 15:36:14 -0700

    Add react-hook-forms and begin implementation for updating card data

--- src/app/cards/CardData.tsx
+++ src/app/cards/CardData.tsx
@@ -1,34 +1,52 @@
-'use client'
+'use client';
 
-import { Box, Flex, Input, Stack, HStack, Button, Checkbox, Text, Textarea } from '@chakra-ui/react'
-import type { FlashCard } from '../../utils/cards'
+import {
+    Box,
+    Flex,
+    FormControl,
+    Input,
+    Stack,
+    HStack,
+    Button,
+    Checkbox,
+    Text,
+    Textarea,
+} from '@chakra-ui/react';
+import { useForm } from 'react-hook-form'
+import type { FlashCard } from '../../utils/cards';
 
 interface Props {
-  selectedCard: FlashCard | null
+    selectedCard: FlashCard | null;
 }
 
-export function CardData(props: Props) {
+export function CardData({ selectedCard }: { selectedCard: FlashCard | null }) {
+    const handleUpdate = () => {
+
+    };
+
     return (
+      <FormControl>
         <Stack className="w-96 p-6 grow" spacing={6}>
             <Stack>
                 <Text>Question</Text>
-                <Input value={props.selectedCard?.question}/>
+                <Input value={selectedCard?.question} />
             </Stack>
 
             <Stack>
                 <Text>Answer</Text>
-                <Textarea value={props.selectedCard?.answer} resize="vertical"/>
+                <Textarea value={selectedCard?.answer} resize="vertical" />
             </Stack>
 
-            <HStack className="mt-5" justify='space-between'>
+            <HStack className="mt-5" justify="space-between">
                 <HStack spacing={5}>
-                  <Checkbox>Learned</Checkbox>
-                  <Checkbox>Format as Code</Checkbox>
+                    <Checkbox>Learned</Checkbox>
+                    <Checkbox>Format as Code</Checkbox>
                 </HStack>
                 <Box>
-                  <Button>Save Changes</Button>
+                    <Button>Save Changes</Button>
                 </Box>
             </HStack>
         </Stack>
+      </FormControl>
     );
 }
--- src/app/cards/actions.ts
+++ src/app/cards/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { prisma } from '@/db';
+import { FlashCard } from '../common/FlashCard';
 
 export const createCard = async () => {
     const createdCard = await prisma.card.create({
@@ -15,10 +16,21 @@ export const createCard = async () => {
 
 export const deleteCard = async (cardID: string) => {
     const deletedCard = await prisma.card.delete({
-        where: { id: cardID },
+      where: { id: cardID },
     });
     return deletedCard;
 };
+
+export const updateCard = async (card: FlashCard) => {
+  const updatedCard = await prisma.card.update({
+    where: { id: card.id },
+    data: {
+      question: card.question,
+      answer: card.answer,
+      learned: card.learned,
+    }
+  })
+}
 
 export type CreateCardResult = Awaited<ReturnType<typeof createCard>>;
 export type DeleteCardResult = Awaited<ReturnType<typeof deleteCard>>;
